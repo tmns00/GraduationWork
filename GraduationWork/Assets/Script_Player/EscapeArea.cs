5451cf453bbe082a669cf42de1c2d4b4fdcd9e4f
ReoMatsumura <reo-hnf-pc@outlook.jp>
2021-11-05 12:07:38 +0900

    出口を実装

---
+++ GraduationWork/Assets/Script_Player/EscapeArea.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeArea : MonoBehaviour
+{
+    [SerializeField]
+    private Treasure treasure;
+    [SerializeField]
+    private GameSystem4 sceneChange;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //‚¨•óæ“¾ó‘Ô‚ÅƒvƒŒƒCƒ„[‚ªN“ü
+        if(other.gameObject.tag == "Player" && treasure.IsGetFlag())
+        {
+            sceneChange.StartGame();
+        }
+    }
+}
