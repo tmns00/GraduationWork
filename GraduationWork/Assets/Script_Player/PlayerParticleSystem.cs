279430c5736189df3af9e3a1db0954e284aeb6a5
ReoMatsumura <reo-hnf-pc@outlook.jp>
2021-11-15 11:01:00 +0900

    強制時のバグ修正

--- GraduationWork/Assets/Script_Player/PlayerParticleSystem.cs
+++ GraduationWork/Assets/Script_Player/PlayerParticleSystem.cs
@@ -27,7 +27,6 @@ public class PlayerParticleSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(ghostSystem.GetIsGhost());
         if (!ghostSystem.GetIsGhost() && !currentGhost)
             return;
 
