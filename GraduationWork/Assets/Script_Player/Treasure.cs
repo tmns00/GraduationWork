bf1a3990aa257ef010644ebe63fb47fb8dc0ec02
ReoMatsumura <reo-hnf-pc@outlook.jp>
2021-11-19 19:31:06 +0900

    β版(修正前)

--- GraduationWork/Assets/Script_Player/Treasure.cs
+++ GraduationWork/Assets/Script_Player/Treasure.cs
@@ -6,24 +6,24 @@ using UnityEngine.UI;
 public class Treasure : MonoBehaviour
 {
     private bool isGet = false;
-    [SerializeField]
-    GameSystem4 toClear;
-    GameObject gauge;
-    [SerializeField] GameObject gauge2;
-    [SerializeField] GameObject text;
+    //[SerializeField]
+    //GameSystem4 toClear;
+    //GameObject gauge;
+    //[SerializeField] GameObject gauge2;
+    //[SerializeField] GameObject text;
 
     private void Start()
     {
-        gauge = GameObject.Find("Canvas");
+        //gauge = GameObject.Find("Canvas");
     }
 
     private void Update()
     {
-        if (isGet)
-        {
-            gauge.SetActive(false);
-            this.gameObject.SetActive(false);
-        }
+        //if (isGet)
+        //{
+        //    gauge.SetActive(false);
+        //    this.gameObject.SetActive(false);
+        //}
         //else
         //{
         //    gauge2.SetActive(false);
@@ -34,8 +34,8 @@ public class Treasure : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
             isGet = true;
-        gauge2.SetActive(true);
-        text.SetActive(true);
+        //gauge2.SetActive(true);
+        //text.SetActive(true);
     }
 
     public bool IsGetFlag()
