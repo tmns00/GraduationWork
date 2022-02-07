e1fcc8b49b279abfc99fc85ed074fa153e9eb30c
ReoMatsumura <reo-hnf-pc@outlook.jp>
2022-02-07 10:50:50 +0900

    当たり判定調整、ボタンのUI表示、センサーを帰りのときに反応しないように、エネミーが動かない不具合修正

--- GraduationWork/Assets/Script/maeda_Script/InfrareSensor.cs
+++ GraduationWork/Assets/Script/maeda_Script/InfrareSensor.cs
@@ -75,7 +75,7 @@ public class InfrareSensor : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!gimkPower) return;
+        if (!gimkPower || RoundTripManager.GetIsBackWay()) return;
 
         if(other.CompareTag("Player"))
         {
